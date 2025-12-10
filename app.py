eb5f3a8627fef64539f6cceea30174f1b3dd477c
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-12-10 08:23:30 +0000

    changed the start cmd

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# import the fastapi library(package)
 from fastapi import FastAPI
-from typing import Union
 from pydantic import BaseModel
 import joblib
 
@@ -10,15 +8,18 @@ app = FastAPI()
 # load model
 model = joblib.load('model.pkl')
 
+
 class InputData(BaseModel):
     X: float
-    
+
 
 @app.get("/")
 def read_root():
     return {"message": "Helo world"}
 
+
 @app.post("/predict")
 def predict(data: InputData):
     prediction = model.predict([[data.X]])[0]
     return {"prediction": prediction}
+
